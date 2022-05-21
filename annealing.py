37c9003fce8cfdede5631525b259ff1faf106479
Mahmoud-Yasser-18 <75978552+Mahmoud-Yasser-18@users.noreply.github.com>
2022-05-21 03:08:33 +0200

    Update annealing.py

--- annealing.py
+++ annealing.py
@@ -1,1 +1,44 @@
 
+import math
+from keras.callbacks import Callback
+from keras import backend as K
+
+
+class CosineAnnealingScheduler(Callback):
+    """Cosine annealing scheduler.
+    """
+
+    def __init__(self, T_max, eta_max, eta_min=0, verbose=1,num_to_start=0,boost=50000000000,boost_rate=0.01):
+        super(CosineAnnealingScheduler, self).__init__()
+        self.T_max = T_max
+        self.eta_max = eta_max
+        self.eta_min = eta_min
+        self.verbose = verbose
+        self.num_to_start = num_to_start
+        self.boost = boost #4
+        self.boost_rate = boost_rate
+
+    def on_epoch_begin(self, epoch, logs=None):
+        if epoch< self.num_to_start:
+            return 
+        if epoch> self.boost_rate and  (epoch % self.boost_rate == 0 or epoch+1 % self.boost_rate == 0 ) :
+            if not hasattr(self.model.optimizer, 'lr'):
+                raise ValueError('Optimizer must have a "lr" attribute.')
+            lr = self.boost
+            K.set_value(self.model.optimizer.lr, lr)
+            if self.verbose > 0:
+                print('\nEpoch %05d: Boosting setting learning '
+                      'rate to %s.' % (epoch + 1, lr))
+            return  
+
+        if not hasattr(self.model.optimizer, 'lr'):
+            raise ValueError('Optimizer must have a "lr" attribute.')
+        lr = self.eta_min + (self.eta_max - self.eta_min) * (1 + math.cos(math.pi * epoch / self.T_max)) / 2
+        K.set_value(self.model.optimizer.lr, lr)
+        if self.verbose > 0:
+            print('\nEpoch %05d: CosineAnnealingScheduler setting learning '
+                  'rate to %s.' % (epoch + 1, lr))
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        logs['lr'] = K.get_value(self.model.optimizer.lr)
