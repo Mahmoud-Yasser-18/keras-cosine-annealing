00c248e587beb50110f05e9029be1a2b96fdbe30
Mahmoud-Yasser-18 <75978552+Mahmoud-Yasser-18@users.noreply.github.com>
2022-05-21 02:37:32 +0200

    Update cosine_annealing.py
    Adding num to start

--- cosine_annealing.py
+++ cosine_annealing.py
@@ -7,21 +7,23 @@ class CosineAnnealingScheduler(Callback):
     """Cosine annealing scheduler.
     """
 
-    def __init__(self, T_max, eta_max, eta_min=0, verbose=0):
+    def __init__(self, T_max, eta_max, eta_min=0, verbose=1,num_to_start=0):
         super(CosineAnnealingScheduler, self).__init__()
         self.T_max = T_max
         self.eta_max = eta_max
         self.eta_min = eta_min
         self.verbose = verbose
+        self.num_to_start = num_to_start
 
     def on_epoch_begin(self, epoch, logs=None):
-        if not hasattr(self.model.optimizer, 'lr'):
-            raise ValueError('Optimizer must have a "lr" attribute.')
-        lr = self.eta_min + (self.eta_max - self.eta_min) * (1 + math.cos(math.pi * epoch / self.T_max)) / 2
-        K.set_value(self.model.optimizer.lr, lr)
-        if self.verbose > 0:
-            print('\nEpoch %05d: CosineAnnealingScheduler setting learning '
-                  'rate to %s.' % (epoch + 1, lr))
+        if epoch>= num_to_start:
+            if not hasattr(self.model.optimizer, 'lr'):
+                raise ValueError('Optimizer must have a "lr" attribute.')
+            lr = self.eta_min + (self.eta_max - self.eta_min) * (1 + math.cos(math.pi * epoch / self.T_max)) / 2
+            K.set_value(self.model.optimizer.lr, lr)
+            if self.verbose > 0:
+                print('\nEpoch %05d: CosineAnnealingScheduler setting learning '
+                      'rate to %s.' % (epoch + 1, lr))
 
     def on_epoch_end(self, epoch, logs=None):
         logs = logs or {}
